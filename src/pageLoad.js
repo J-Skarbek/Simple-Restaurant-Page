bea28e9e0f5541bf51a925f5dc868dee966bb32a
J-Skarbek <john@snarkmediaagency.com>
2022-09-27 15:01:08 -0500

    Add body text for first item and work on basic styling.

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,6 +1,6 @@
 import { default as header } from './header.js'
-import { default as body } from './body.js'
 import { default as sidebar } from './sidebar.js'
+import { default as body } from './body.js'
 
 export default function pageLoad() {
   header()
