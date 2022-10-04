3e242c4ee410165bfefcd158ae4269bf9428878e
J-Skarbek <john@snarkmediaagency.com>
2022-10-04 14:53:59 -0500

    Add some dishes to their own modules and test the loading.

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,11 +1,15 @@
 import { default as header } from './header.js'
 import { default as sidebar } from './sidebar.js'
-import { default as body } from './body.js'
+// import { default as body } from './body.js'
 import { default as footer } from './footer.js'
+// import { default as linguine} from './linguine.js'
+import { default as frenchToast} from './french-toast.js'
 
 export default function pageLoad() {
   header()
   sidebar()
-  body()
+//   body()
+//   linguine()
+  frenchToast()
   footer()
 }
