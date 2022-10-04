e503aa415c54e71059a4762f7fe4e351553ba94e
J-Skarbek <john@snarkmediaagency.com>
2022-10-04 15:15:50 -0500

    Add other menu items to their own modules.

---
+++ src/eggs.js
@@ -0,0 +1,37 @@
+import Eggs from './assets/images/fancy-eggs.jpg'
+
+export default function bodyComponent() {
+  const element = document.createElement('div')
+  element.classList.add('main-content')
+
+  const eggsImg = new Image()
+  eggsImg.src = Eggs
+  eggsImg.classList.add('featured-img')
+
+  const detailsBox = document.createElement('div')
+  detailsBox.classList.add('details-box')
+  
+  const dishName = document.createElement('h2')
+  dishName.innerText = 'Fancy Filet'
+
+  const dishPrice = document.createElement('h3')
+  dishPrice.innerText = '$14.99'
+
+  const dishDetails = document.createElement('div')
+  const detailsTextBody = document.createElement('p')
+  detailsTextBody.classList.add('dish-details-text')
+  dishDetails.classList.add('dish-deatils')
+  detailsTextBody.innerText = 'The Fancy Eggs Meal is ... Enim nec dui nunc mattis enim. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien. Nec feugiat in fermentum posuere urna nec. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Praesent semper feugiat nibh sed pulvinar proin. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget. Amet justo donec enim diam. Vestibulum rhoncus est pellentesque elit. Cras ornare arcu dui vivamus arcu felis bibendum. Sociis natoque penatibus et magnis. Dapibus ultrices in iaculis nunc sed augue. Aliquam id diam maecenas ultricies. Tempus quam pellentesque nec nam aliquam sem et. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Auctor elit sed vulputate mi. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Ac orci phasellus egestas tellus rutrum tellus. Tempor orci dapibus ultrices in. Sed faucibus turpis in eu mi bibendum neque. Elementum facilisis leo vel fringilla est ullamcorper eget nulla.'
+
+  element.appendChild(eggsImg)
+  
+  element.appendChild(detailsBox)
+  detailsBox.appendChild(dishName)
+  detailsBox.appendChild(dishPrice)
+  detailsBox.appendChild(dishDetails)
+  dishDetails.appendChild(detailsTextBody)
+  
+  return element 
+}
+  
+document.body.appendChild(bodyComponent());
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -2,14 +2,18 @@ import { default as header } from './header.js'
 import { default as sidebar } from './sidebar.js'
 // import { default as body } from './body.js'
 import { default as footer } from './footer.js'
-// import { default as linguine} from './linguine.js'
+import { default as linguine} from './linguine.js'
 import { default as frenchToast} from './french-toast.js'
+import { default as filet} from './filet.js'
+import { default as eggs} from './eggs.js'
 
 export default function pageLoad() {
   header()
   sidebar()
 //   body()
-//   linguine()
+  linguine()
   frenchToast()
+  filet()
+  eggs()
   footer()
 }
