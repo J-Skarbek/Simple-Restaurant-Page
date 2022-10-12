9a20efa834de3b25833efc3b02f6e86a1729a5bf
J-Skarbek <john@snarkmediaagency.com>
2022-10-12 11:36:31 -0500

    Clean up code and remove unnecesary commeents.

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,23 +1,17 @@
 import { default as header } from './header.js'
 import { default as sidebar } from './sidebar.js'
-import { default as body } from './body.js'
 import { default as footer } from './footer.js'
 // import { default as linguine} from './linguine.js'
 // import { default as frenchToast} from './french-toast.js'
 // import { default as filet} from './filet.js'
 import { default as eggs} from './eggs.js'
 
-// const mainContent = document.querySelector('.content')
+
 
 export default function pageLoad() {
   header()
   document.body.appendChild(header());
   sidebar()
-  body()
-//   linguine()
-//   frenchToast()
-//   filet()
   eggs()
-//   document.body.appendChild(eggs());
   footer()
 }
